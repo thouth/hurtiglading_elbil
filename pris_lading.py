6d955162f6fc1d10eddebdf4350b6cdaafd0ab44
Thomas Uthaug <thomasuthaug@Thomass-MacBook-Pro-2.local>
2021-11-02 21:44:35 +0100

    lagt til data

---
+++ pris_lading.py
@@ -0,0 +1,116 @@
+pris_lading = {
+    "lynlading":[
+        {
+        "id": "1",
+        "navn": "Fortum",
+        "kr/kWh": 5,
+        "kr/min": 0
+        },
+        {
+        "id": "2",
+        "navn": "Mer",
+        "kr/kWh": 3.75,
+        "kr/min": 1.25
+        },
+        {
+        "id": "3",
+        "navn": "BKK",
+        "kr/kWh": 3.65,
+        "kr/min": 1.25
+        },
+        {
+        "id": "4",
+        "navn": "Lyse",
+        "kr/kWh": 3.65,
+        "kr/min": 1.25
+        },
+        {
+        "id": "5",
+        "navn": "CircleK",
+        "kr/kWh": 4.99,
+        "kr/min": 0
+        },
+        {
+        "id": "6",
+        "navn": "Kople",
+        "kr/kWh": 3.2,
+        "kr/min": 1.25
+        },
+        {
+        "id": "7",
+        "navn": "Supercharge",
+        "kr/kWh": 3.2,
+        "kr/min": 1.25
+        },
+        {
+        "id": "8",
+        "navn": "E.ON",
+        "kr/kWh": 5,
+        "kr/min": 0
+        },
+        {
+        "id": "9",
+        "navn": "Ionity",
+        "kr/kWh": 8.4,
+        "kr/min": 0
+        }
+    ],
+    "hurtiglading": [
+    {
+        "id": "1",
+        "navn": "Fortum",
+        "kr/kWh": 3.1,
+        "kr/min": 0
+        },
+        {
+        "id": "2",
+        "navn": "Mer",
+        "kr/kWh": 3.75,
+        "kr/min": 1.25
+        },
+        {
+        "id": "3",
+        "navn": "BKK",
+        "kr/kWh": 3.65,
+        "kr/min": 1.25
+        },
+        {
+        "id": "4",
+        "navn": "Lyse",
+        "kr/kWh": 3.65,
+        "kr/min": 1.25
+        },
+        {
+        "id": "5",
+        "navn": "CircleK",
+        "kr/kWh": 4.49,
+        "kr/min": 0
+        },
+        {
+        "id": "6",
+        "navn": "Kople",
+        "kr/kWh": 3.2,
+        "kr/min": 1
+        },
+        {
+        "id": "7",
+        "navn": "Supercharge",
+        "kr/kWh": 3.2,
+        "kr/min": 1.25
+        },
+        {
+        "id": "8",
+        "navn": "E.ON",
+        "kr/kWh": 4,
+        "kr/min": 0
+        },
+        {
+        "id": "9",
+        "navn": "Ionity",
+        "kr/kWh": 3.75,
+        "kr/min": 1.25
+        }
+    ]
+}
+
+
