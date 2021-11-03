7dbc820d8305f65e2ba783d152d77fc7200e2d43
thouth <78444186+thouth@users.noreply.github.com>
2021-11-03 12:26:29 +0100

    ny dict

--- kalkulere_prisomtimum.py
+++ kalkulere_prisomtimum.py
@@ -1,122 +1,28 @@
 #from pris_lading import pris_lading
 
-pris_lading = {
-    "lynlading":[
-        {
-        "id": "1",
-        "navn": "Fortum",
-        "kr/kWh": 5,
-        "kr/min": 0
-        },
-        {
-        "id": "2",
-        "navn": "Mer",
-        "kr/kWh": 3.75,
-        "kr/min": 1.25
-        },
-        {
-        "id": "3",
-        "navn": "BKK",
-        "kr/kWh": 3.65,
-        "kr/min": 1.25
-        },
-        {
-        "id": "4",
-        "navn": "Lyse",
-        "kr/kWh": 3.65,
-        "kr/min": 1.25
-        },
-        {
-        "id": "5",
-        "navn": "CircleK",
-        "kr/kWh": 4.99,
-        "kr/min": 0
-        },
-        {
-        "id": "6",
-        "navn": "Kople",
-        "kr/kWh": 3.2,
-        "kr/min": 1.25
-        },
-        {
-        "id": "7",
-        "navn": "Supercharge",
-        "kr/kWh": 3.2,
-        "kr/min": 1.25
-        },
-        {
-        "id": "8",
-        "navn": "E.ON",
-        "kr/kWh": 5,
-        "kr/min": 0
-        },
-        {
-        "id": "9",
-        "navn": "Ionity",
-        "kr/kWh": 8.4,
-        "kr/min": 0
-        }
-    ],
-    "hurtiglading": [
-    {
-        "id": "1",
-        "navn": "Fortum",
-        "kr/kWh": 3.1,
-        "kr/min": 0
-        },
-        {
-        "id": "2",
-        "navn": "Mer",
-        "kr/kWh": 3.75,
-        "kr/min": 1.25
-        },
-        {
-        "id": "3",
-        "navn": "BKK",
-        "kr/kWh": 3.65,
-        "kr/min": 1.25
-        },
-        {
-        "id": "4",
-        "navn": "Lyse",
-        "kr/kWh": 3.65,
-        "kr/min": 1.25
-        },
-        {
-        "id": "5",
-        "navn": "CircleK",
-        "kr/kWh": 4.49,
-        "kr/min": 0
-        },
-        {
-        "id": "6",
-        "navn": "Kople",
-        "kr/kWh": 3.2,
-        "kr/min": 1
-        },
-        {
-        "id": "7",
-        "navn": "Supercharge",
-        "kr/kWh": 3.2,
-        "kr/min": 1.25
-        },
-        {
-        "id": "8",
-        "navn": "E.ON",
-        "kr/kWh": 4,
-        "kr/min": 0
-        },
-        {
-        "id": "9",
-        "navn": "Ionity",
-        "kr/kWh": 3.75,
-        "kr/min": 1.25
-        }
-    ]
-}
+lynlading =     {
+        "Fortum":       {"kr_kWh": 5,       "kr_min": 0},
+        "Mer":          {"kr_kWh": 3.75,    "kr_min": 1.25 },
+        "BKKLyse":      {"kr_kWh": 3.65,    "kr_min": 1.25},
+        "CircleK":      {"kr_kWh": 4.99,    "kr_min": 0},
+        "Kople":        {"kr_kWh": 3.2,     "kr_min": 1.25},
+        "Supercharge":  {"kr_kWh": 3.2,     "kr_min": 1.25},
+        "Eon":          {"kr_kWh": 5,       "kr_min": 0},
+        "Ionity":       {"kr_kWh": 8.4,     "kr_min": 0}
+                }
+
+hurtiglading =  {
+        "Fortum":       {"kr_kWh": 3.1,       "kr_min": 0},
+        "Mer":          {"kr_kWh": 3.75,    "kr_min": 1.25 },
+        "BKKLyse":      {"kr_kWh": 3.65,    "kr_min": 1.25},
+        "CircleK":      {"kr_kWh": 4.49,    "kr_min": 0},
+        "Kople":        {"kr_kWh": 3.2,     "kr_min": 1},
+        "Supercharge":  {"kr_kWh": 3.2,     "kr_min": 1.25},
+        "Eon":          {"kr_kWh": 4,       "kr_min": 0},
+        "Ionity":       {"kr_kWh": 3.75,     "kr_min": 1.25}
+                }
+
 
 kWh_ladet = [50,100,150,200,250,300,400,500,600,700,800,900,1000,1250,1500,1750,2000]
 ladehastighet = [50,100]
-min_lader = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,45,50,55,60]
-
-print(pris_lading)
+minutter_ladet = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,45,50,55,60]
